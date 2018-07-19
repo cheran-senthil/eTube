d92ac5f6b28a8df066965d1fd3329052250b081f
Cheran Senthil <cheran.v.senthil@gmail.com>
2018-07-19 15:53:07 +0530

    Update youtube-api.js

--- server/youtube-api.js
+++ server/youtube-api.js
@@ -1,13 +1,13 @@
 var https = require('https')
 var fs = require('fs')
 
-function downloadData(url) {
+function downloadJSON(url) {
   return new Promise((resolve, reject) => {
     https.get(url, (response) => {
       var data = ''
 
       response.on('data', (chunk) => { data += chunk })
-      response.on('end', () => { resolve(data) })
+      response.on('end', () => { resolve(JSON.parse(data)) })
       response.on('error', reject)
     })
   })
@@ -16,12 +16,12 @@ function downloadData(url) {
 function channelRequest(id, key, maxResults) {
   maxResults = maxResults || 20
   let url = `https://www.googleapis.com/youtube/v3/search?key=${key}&channelId=${id}&part=snippet,id&order=date&maxResults=${maxResults}`
-  return downloadData(url)
+  return downloadJSON(url)
 }
 
 function videoRequest(id, key) {
   let url = `https://www.googleapis.com/youtube/v3/videos?part=snippet&type=video&id=${id}&key=${key}`
-  return downloadData(url)
+  return downloadJSON(url)
 }
 
 module.exports = {channelRequest: channelRequest, videoRequest: videoRequest}
