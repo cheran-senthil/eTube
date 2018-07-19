e8811cda096b9b95e687d64cdadfe2bdaab6bee1
Cheran Senthil <cheran.v.senthil@gmail.com>
2018-07-19 15:30:52 +0530

    Create youtube-api.js

---
+++ server/youtube-api.js
@@ -0,0 +1,26 @@
+var https = require('https')
+var fs = require('fs')
+
+function downloadData(url) {
+  return new Promise((resolve, reject) => {
+    https.get(url, (response) => {
+      var data = ''
+
+      response.on('data', (chunk) => { data += chunk })
+      response.on('end', () => { resolve(data) })
+      response.on('error', reject)
+    })
+  })
+}
+
+function channelRequest(id, key) {
+  let url = `https://www.googleapis.com/youtube/v3/search?key=${key}&channelId=${id}&part=snippet,id&order=date&maxResults=20`
+  return downloadData(url)
+}
+
+function videoRequest(id, key) {
+  let url = `https://www.googleapis.com/youtube/v3/videos?part=snippet&type=video&id=${id}&key=${key}`
+  return downloadData(url)
+}
+
+module.exports = {channelRequest: channelRequest, videoRequest: videoRequest}
