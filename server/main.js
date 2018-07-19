66a5f34a7f6b27e4c3580927f6527d34e03b0011
Cheran Senthil <cheran.v.senthil@gmail.com>
2018-07-19 17:00:32 +0530

    Load videos to mongodb

--- server/main.js
+++ server/main.js
@@ -1,5 +1,25 @@
 import { Meteor } from 'meteor/meteor';
+import { Mongo } from 'meteor/mongo';
+import { channelRequest, videoRequest } from './youtube-api'
+
+function loadMongo() {
+  const videoDB = new Mongo.Collection('videodb')
+  videoDB.remove({})
+
+  let key = ''
+  var channels = 'UCHnyfMqiRRG1u-2MsSQLbXA' // Veritasium
+
+  channelRequest(channels, key, 100).then((videos) => {
+    videos.items.forEach((video) => {
+      var videoId = video.id.videoId
+      videoRequest(videoId, key).then((data) => {
+        videoDB.insert(data.items[0])
+      })
+    })
+  })
+}
+
 
 Meteor.startup(() => {
-  // code to run on server at startup
-});
+  loadMongo()
+})
