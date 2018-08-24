a01ac8ff2f09ef2607642b15b031566fc7f98e6e
mukundan314 <mukundan314@github.com>
2018-08-24 13:48:24 +0530

    Add search bar

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSearch } from '@fortawesome/free-solid-svg-icons'
 
 const App = props => {
   return (
     <div>
+      <form action="/search">
+        <input type="text" placeholder="Search.." name="query" />
+        <button type="submit"><FontAwesomeIcon icon={faSearch} /></button>
+      </form>
       {props.content}
     </div>
   )
